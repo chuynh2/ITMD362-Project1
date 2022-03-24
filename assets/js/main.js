b4d2d899c5f7b3be138fffbddf1705052903f21e
Christine <chuynh2@hawk.iit.edu>
2022-03-24 12:17:37 -0500

    Update link for stylesheet

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,12 @@
 document.querySelector('html').className = 'js';
 
+try{
+
+var shoppingContButton = document.getElementById('cont-shopping');
+
+contOneButton.addEventListener('click', function(){
+  location.href = "../payment/index.html";
+});
+}
+catch(err){
+}
