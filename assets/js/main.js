2f1baaa33e5c6969669b2f6dd4bfcad37db6f2fd
Christine <chuynh2@hawk.iit.edu>
2022-03-24 17:29:57 -0500

    Create dropdown for state and country with style

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,1 @@
 document.querySelector('html').className = 'js';
-
-try{
-
-var shoppingContButton = document.getElementById('cont-shopping');
-
-contOneButton.addEventListener('click', function(){
-  location.href = "../payment/index.html";
-});
-}
-catch(err){
-}
