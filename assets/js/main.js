ff63105a27ea3fdbbe1e72326987edb677371a70
Christine <chuynh2@hawk.iit.edu>
2022-03-24 21:30:16 -0500

    Add JS to hide shipping address form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
-document.querySelector('html').className = 'js';
+var x = document.getElementById("shippingForm");
+x.style.display = "none";
+function showDifferentAddress() {
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
