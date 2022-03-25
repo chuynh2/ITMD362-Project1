9a2f3540cc6a1696338ee8fc11c9983263880c32
Christine <chuynh2@hawk.iit.edu>
2022-03-24 21:36:49 -0500

    Add js for back and continue buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,20 @@ var x = document.getElementById("shippingForm");
 x.style.display = "none";
 function showDifferentAddress() {
   if (x.style.display === "none") {
-    x.style.display = "block";
+      x.style.display = "block";
   } else {
-    x.style.display = "none";
+      x.style.display = "none";
   }
+}
+function ctnPayment(){
+    location.href='payment/index.html';
+}
+function backShopping(){
+    location.href='../index.html';
+}
+function ctnShipping(){
+    location.href='../shipping/index.html';
+}
+function backPayment(){
+    location.href='../payment/index.html';
 }
